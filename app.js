95be32af5931fd767b7b745a660a5d9871248eb2
bhargavvarmasagi <bhargavvarmasvs2001@gmail.com>
2024-03-23 20:29:01 -0500

    done

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ballsRouter = require('./routes/Balls');
-var gridRouter = require('./routes/grid');
+// var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick')
 
 var app = express();
 
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Balls',ballsRouter);
-app.use('/grid',gridRouter);
+// app.use('/grid',gridRouter);
+app.use('/pick', pickRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
